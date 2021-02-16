530a9ddfb7b371a3d2c88bfd3d3b8eef4930c3e6
Hector <hectormrg0823@gmail.com>
2021-02-16 06:24:37 -0800

    add project skeleton

---
+++ src/main/java/com/architectproject/ArchitectProj/ArchitectProjApplication.java
@@ -0,0 +1,13 @@
+package com.architectproject.ArchitectProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitectProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/architectproject/ArchitectProj/ArchitectProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.architectproject.ArchitectProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
